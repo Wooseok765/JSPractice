bf011540725a3948e6be350d5f55183d19041b8f
Wooseok765 <dntjr7580@gmail.com>
2026-06-06 15:30:12 +0100

    function

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -1,9 +1,29 @@
-const player = {
-    name : "Wooseok",
-    point: 15648,
-    feature : "handsome"
+/*
+function sayHello(a){
+    console.log("Hello! ", a);
 }
-console.log(player);
-player.feature = "ugly";
-player.lastName = "Bang";
-console.log(player);
+
+sayHello("Wooseok");
+sayHello("Nicco");
+
+function plus(a=0, b=0){
+    console.log(a + b);
+}
+
+plus(18, 79);
+plus();
+
+function divide(a=0, b=1){
+    console.log(a / b);
+}
+divide(9, 5)
+*/
+
+const player={
+    name: "Wooseok",
+    sayHello: function(a){
+        console.log("hello!", a);
+    },
+};
+console.log(player.name);
+player.sayHello(player.name);
