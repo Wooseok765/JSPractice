5e5364bddb5fc15b30953adf1f21074d94d23633
Wooseok765 <dntjr7580@gmail.com>
2026-06-06 20:39:25 +0100

    invoking2

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -2,18 +2,21 @@ const hellos = document.getElementsByClassName("hello");
 console.log(hellos);
 
 const title = document.getElementsByTagName("h1");
-console.log(title)
+console.log("title", title)
 
-const title2 = document.querySelector("h1.hello");
-console.log("sellector", title2)
+const title2 = document.querySelector("div.hello:first-child h1");
+console.log("sellector", typeof title2)
 // Call elements taged h1 from the parent elements that class name is hello
 // The parent is not being called
 // It is searching a CSS selector like h1
 // Able to use without classname or id
 
 const title3 = document.querySelectorAll(".hello h1");
-console.log(title3)
+console.dir(title3)
+// return objects of title3
 
 title2.innerText = "shut up";
 // This isn't applied to .querySelectorAll
 // or functions that return more than one element
+
+title2.style.color="red";
