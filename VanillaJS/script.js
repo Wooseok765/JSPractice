2e274ab1aabef2a0f975833b0eae1fb0a8691111
Wooseok765 <dntjr7580@gmail.com>
2026-06-06 19:44:30 +0100

    get element by tag, id, class, selector

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -1,5 +1,19 @@
-const title = document.getElementById("title");
-console.log(title);
-title.innerText = "Got you!";
-console.log(title.id);
-console.log(title.className);
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos);
+
+const title = document.getElementsByTagName("h1");
+console.log(title)
+
+const title2 = document.querySelector("h1.hello");
+console.log("sellector", title2)
+// Call elements taged h1 from the parent elements that class name is hello
+// The parent is not being called
+// It is searching a CSS selector like h1
+// Able to use without classname or id
+
+const title3 = document.querySelectorAll(".hello h1");
+console.log(title3)
+
+title2.innerText = "shut up";
+// This isn't applied to .querySelectorAll
+// or functions that return more than one element
