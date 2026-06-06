2ace396feb62432cb3cdb419f19d74a5d88615a3
Wooseok765 <dntjr7580@gmail.com>
2026-06-06 19:08:59 +0100

    invoking html from JS

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -1,1 +1,5 @@
-document.title = "hello from js";
+const title = document.getElementById("title");
+console.log(title);
+title.innerText = "Got you!";
+console.log(title.id);
+console.log(title.className);
