d74bd915673203cecd95e65d26073680c38f472d
Wooseok765 <dntjr7580@gmail.com>
2026-06-07 16:39:53 +0100

    control form(submit)

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -1,30 +1,18 @@
-const loginForm = document.getElementById("login-form");
 
-const loginInput = loginForm.querySelector("input");
-// It returns the first matching element directly
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-/*
-const loginButton = loginForm.getElementsByTagName("button");
-// It returns HTML collection (object) even if there is only one 
-// So thin code will look up button elements inside of the loginForm 
-// then return a list of buttons
-// so does getElementByClassName
-// It can't call eventListener
-*/
-const loginButton = loginForm.querySelector("button");
 
-function handleButtonClick() {
-    
+
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    // It is blocking an automatic submit
+    // then the website is not going to refresh after submiting
     const userName = loginInput.value;
-    /*
-    if (userName === "") { form 에서 사용 가능한 required를 대체
-        alert("Please, enter a name");
-    } else if (userName.length > 15) { form에서 사용가능한 maxlength를 대체
-        alert("Your name is sucks")
-    } else {
-        alert("Hello, " + userName);
-    }
-    */
+    console.log(event);
+
 }
 
-loginButton.addEventListener("click", handleButtonClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+// addEventListener send arguements to the target
