e1276a1006daa1fe7f674f76d6598127d32e3347
Wooseok765 <dntjr7580@gmail.com>
2026-06-07 21:23:21 +0100

    랜덤명언올리기

---
+++ VanillaJS/JS/quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+    {
+        quote: "The only way to do great work is to love what you do",
+        author: "Steve Jobs"
+    },
+    {
+        quote: "Life is what happens when you’re busy making other plans",
+        author: "John Lennon"
+    },
+    {
+        quote: "It does not matter how slowly you go as long as you do not stop",
+        author: "Confucius"
+    },
+    {
+        quote: "In the middle of difficulty lies opportunity",
+        author: "Albert Einstein"
+    },
+    {
+        quote: "Do what you can, with what you have, where you are",
+        author: "Theodore Roosevelt"
+    },
+    {
+        quote: "Success is not final, failure is not fatal: it is the courage to continue that counts",
+        author: "Winston Churchill"
+    },
+    {
+        quote: "The journey of a thousand miles begins with a single step",
+        author: "Lao Tzu"
+    },
+    {
+        quote: "Believe you can and you’re halfway there",
+        author: "Theodore Roosevelt"
+    },
+    {
+        quote: "Happiness depends upon ourselves",
+        author: "Aristotle"
+    },
+    {
+        quote: "Dream big and dare to fail",
+        author: "Norman Vaughan"
+    },
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)]
+quote.innerText = todaysQuote.quote;
+author.innerText = `-- ${todaysQuote.author}--`;
+// 0이상 1미만의 랜덤숫자와 array 내부 저장된 element 숫자를 곱하면 
+// 가장 높은 인덱스와(lemgth -1) 0사이의 숫자가 나옴
