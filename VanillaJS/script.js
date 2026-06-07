08730bac7d4f180f3e79ce1f4c7317eb982c89b7
Wooseok765 <dntjr7580@gmail.com>
2026-06-07 17:24:53 +0100

    getting user name

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -1,18 +1,18 @@
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const h1 = document.body.querySelector("h1");
 
-
-
+const HIDDNE_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    // It is blocking an automatic submit
-    // then the website is not going to refresh after submiting
+    loginForm.classList.add(HIDDNE_CLASSNAME);
     const userName = loginInput.value;
-    console.log(event);
-
+    // h1.innerText = "Hello, " + userName;
+    h1.innerText = `Hello, ${userName}`; // Same function as a line above
+    h1.classList.remove(HIDDNE_CLASSNAME);
 }
+//Changes the name of Form then it will disappear because of CSS
 
 loginForm.addEventListener("submit", onLoginSubmit);
-// addEventListener send arguements to the target
