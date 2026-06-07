e4a15b7dd17f7ee1276ad1a5a0d46a78cfd1d1bf
Wooseok765 <dntjr7580@gmail.com>
2026-06-07 19:37:46 +0100

    local storage

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -4,15 +4,35 @@ const loginInput = document.querySelector("#login-form input");
 const h1 = document.body.querySelector("h1");
 
 const HIDDNE_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
-    event.preventDefault();
+    // event.preventDefault();
     loginForm.classList.add(HIDDNE_CLASSNAME);
     const userName = loginInput.value;
-    // h1.innerText = "Hello, " + userName;
-    h1.innerText = `Hello, ${userName}`; // Same function as a line above
+    localStorage.setItem(USERNAME_KEY, userName);
+    // saving user name in the browser
+    // required a key word and value
+
+    paintGreeting(userName);
+    // 새로 입력하는 이름정보로 function 실행
+    // 아래 if statement에서 onLoginSubmit()이 호출되면서 유저가 input에 입력한
+    // 이름정보가 들어옴
+}
+
+function paintGreeting(username) {
+    h1.innerText = `Hello, ${username}`;
     h1.classList.remove(HIDDNE_CLASSNAME);
 }
-//Changes the name of Form then it will disappear because of CSS
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const storedUserName = localStorage.getItem(USERNAME_KEY); // 모든 기능의 시작점
+
+if (storedUserName === null) {
+    loginForm.classList.remove(HIDDNE_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(storedUserName);
+    // local storage에서 넘어오는 이름정보로 function 실행
+}
+
+
