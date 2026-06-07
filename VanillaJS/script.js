9f4ac6e3e7898277123034d7100559f074923fa6
Wooseok765 <dntjr7580@gmail.com>
2026-06-07 14:33:00 +0100

    user name validation

--- VanillaJS/script.js
+++ VanillaJS/script.js
@@ -1,11 +1,30 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.getElementById("login-form");
 
+const loginInput = loginForm.querySelector("input");
+// It returns the first matching element directly
 
-function handleTitleClick() {
-    const clickedClass = "active";
-    console.log(h1.classList.toggle(clickedClass));
-    // If the token, that toggle method is parsing, exists already in the element,
-    // it return false and remove the token or vise versa
+/*
+const loginButton = loginForm.getElementsByTagName("button");
+// It returns HTML collection (object) even if there is only one 
+// So thin code will look up button elements inside of the loginForm 
+// then return a list of buttons
+// so does getElementByClassName
+// It can't call eventListener
+*/
+const loginButton = loginForm.querySelector("button");
+
+function handleButtonClick() {
+    
+    const userName = loginInput.value;
+    /*
+    if (userName === "") { form 에서 사용 가능한 required를 대체
+        alert("Please, enter a name");
+    } else if (userName.length > 15) { form에서 사용가능한 maxlength를 대체
+        alert("Your name is sucks")
+    } else {
+        alert("Hello, " + userName);
+    }
+    */
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", handleButtonClick);
